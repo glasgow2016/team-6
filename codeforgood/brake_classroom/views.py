a7e6e1dbf92bc224823bff78edc49523cedc4b1a
georgikoyrushki95 <georgi.koyrushki@gmail.com>
2016-11-05 21:10:44 +0000

    removed unneeded stuff

--- codeforgood/brake_classroom/views.py
+++ codeforgood/brake_classroom/views.py
@@ -2,18 +2,16 @@ from django.shortcuts import render
 
 
 def index(request):
-
     return render(request, 'brake_classroom/index.html')
 
-def walking(request):
 
+def walking(request):
     return render(request, 'brake_classroom/walking.html')
 
-def cycling(request):
 
+def cycling(request):
     return render(request, 'brake_classroom/cycling.html')
 
 
 def project(request):
     return render(request, 'brake_classroom/project.html')
-
