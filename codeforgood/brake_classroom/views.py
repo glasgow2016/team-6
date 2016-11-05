7989ad7b51f30bdfff7678e8ef11b987f2a66a7d
veselin <v.j.vasilev95@gmail.com>
2016-11-05 17:07:26 +0000

    Set up initial url structure and blank pages

--- codeforgood/brake_classroom/views.py
+++ codeforgood/brake_classroom/views.py
@@ -5,3 +5,14 @@ def index(request):
 
     return render(request, 'brake_classroom/index.html')
 
+def walking(request):
+
+    return render(request, 'brake_classroom/walking.html')
+
+def cycling(request):
+
+    return render(request, 'brake_classroom/cycling.html')
+
+
+def project(request):
+    return render(request, 'brake_classroom/project.html')
