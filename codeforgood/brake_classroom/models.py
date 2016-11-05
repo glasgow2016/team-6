7d5a0c46a135b6675b4f1d9c3dee1f847e8a372c
katya1995 <katq1995@gmail.com>
2016-11-05 19:04:21 +0000

    added models for questions and levels

--- codeforgood/brake_classroom/models.py
+++ codeforgood/brake_classroom/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Level(models.Model):
+    level = models.IntegerField()
+
+class Question(models.Model):
+    question_title = models.TextField(max_length=500)
+    right_answer = models.CharField(max_length=128)
+    wrong_answer1 = models.CharField(max_length=128)
+    wrong_answer2 = models.CharField(max_length=128)
+    wrong_answer3 = models.CharField(max_length=128)
+    popup = models.TextField(max_length=500)
+    level = models.ForeignKey(Level)
+
+    def __unicode__(self):
+        return self.question_title
+
