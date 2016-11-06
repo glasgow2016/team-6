d2598e0a048749366cbd85363938d5a696b325b1
georgikoyrushki95 <georgi.koyrushki@gmail.com>
2016-11-06 00:21:56 +0000

    login

--- codeforgood/brake_classroom/views.py
+++ codeforgood/brake_classroom/views.py
@@ -1,28 +1,45 @@
 from django.shortcuts import render
-from brake_classroom.models import Question, Level
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
+from django.contrib.auth import logout
+from brake_classroom.models import Question
 
 
 def index(request):
-
     return render(request, 'brake_classroom/index.html')
 
-def walking(request):
 
+def walking(request):
     return render(request, 'brake_classroom/walking.html')
+    #
+    # def quiz(request):
+    #     questions = Question.objects.all()
+
 
 def quiz(request):
-
     questions = Question.objects.order_by('number')
     if request.is_ajax():
         pass
-        #answer = request.GET['answer']
+        # answer = request.GET['answer']
     return render(request, 'brake_classroom/quiz.html', {'questions': questions})
 
-def cycling(request):
 
+def cycling(request):
     return render(request, 'brake_classroom/cycling.html')
 
 
 def project(request):
     return render(request, 'brake_classroom/project.html')
 
+
+def login_user(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    login(request, user)
+    return redirect('/')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
