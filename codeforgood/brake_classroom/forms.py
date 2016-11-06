a9780a4abd4c6563e64feb8f39a9cc8be76f9516
georgikoyrushki95 <georgi.koyrushki@gmail.com>
2016-11-06 09:56:54 +0000

    deleted code in forms.py- we don't use it anymore

--- codeforgood/brake_classroom/forms.py
+++ codeforgood/brake_classroom/forms.py
@@ -1,8 +1,1 @@
-from django import forms
-from brake_classroom.models import Project
 
-
-class ProjectForm(forms.ModelForm):
-    class Meta:
-        model = Project
-        exclude = ['created', 'co2_saved', 'money_saved']
