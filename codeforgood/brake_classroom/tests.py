dc56068a7c3ed6df4d4b81bcf456239f1bf4911a
georgikoyrushki95 <georgi.koyrushki@gmail.com>
2016-11-06 09:35:05 +0000

    fixture for question models created

--- codeforgood/brake_classroom/tests.py
+++ codeforgood/brake_classroom/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from brake_classroom.models import Question
 
-# Create your tests here.
+
+class QuestionModelTest(TestCase):
+
+    def setUp(self):
+        """
+        Create fixture for the Question model
+        """
+        question1 = Question(
+            level="walking",
+            number="1",
+            question_title="Is it a good idea to walk?",
+            right_answer="Yes",
+            wrong_answer1="No",
+            wrong_answer2="No",
+            wrong_answer3="No"
+        )
+        question1.save()
+
+        question2 = Question(
+            level="walking",
+            number="2",
+            question_title="Is it a good idea to run?",
+            right_answer="Yes",
+            wrong_answer1="No",
+            wrong_answer2="No",
+            wrong_answer3="No"
+        )
+        question2.save()
+        
